5cae962f6a3a24776036b2648416c37658f6b26e
Caio Luiz <caioriberio@gmail.com>
2026-07-03 10:32:54 -0300

    Ajustes_finais
    Finalizando o projeto e criando um método de deploy

---
+++ Deploy_airbnb.py
@@ -0,0 +1,51 @@
+# Notas para quem for testar:
+# Para rodar esse arquivo, baixe o anaconda3 e execute o arquivo Deploy_airbnb.py por lá
+# Abra o prompt do anaconda3, use cd (caminho do arquivo) para chegar até a pasta do arquivo
+# Use streamlit run Deploy_airbnb.py para rodar o arquivo, após isso irá abrir no navegador toda parte visual
+
+import pandas as pd
+import streamlit as st
+import joblib
+
+# modelo = joblib.load('modelo.joblib')
+
+x_numericos = {'latitude': 0, 'longitude': 0, 'accommodates': 0, 'bathrooms': 0, 'bedrooms': 0, 'beds': 0,
+               'extra_people': 0,
+               'minimum_nights': 0, 'ano': 0, 'mes': 0, 'n_amenities': 0, 'host_listings_count': 0}
+
+x_tf = {'host_is_superhost': 0, 'instant_bookable': 0}
+
+x_listas = {
+    'property_type': ['Apartment', 'Bed and breakfast', 'Condominium', 'Guest suite', 'Guesthouse', 'Hostel', 'House',
+                      'Loft', 'Outros', 'Serviced apartment'],
+    'room_type': ['Entire home/apt', 'Hotel room', 'Private room', 'Shared room'],
+    'cancelation_policy': ['flexible', 'moderate', 'strict', 'strict_14_with_grace_period']
+    }
+
+dicionario = {}
+for item in x_listas:
+    for valor in x_listas[item]:
+        dicionario[f'{item}_{valor}'] = 0
+
+for item in x_numericos:
+    if item == 'latitude' or item == 'longitude':
+        valor = st.number_input(f'{item}', step=0.00001, value=0.0, format='%.5f')
+    elif item == 'extra_people':
+        valor = st.number_input(f'{item}', step=0.01, value=0.0)
+    else:
+        valor = st.number_input(f'{item}', step=1, value=0)
+    x_numericos[item] = valor
+
+for item in x_tf:
+    valor = st.selectbox(f'{item}', ('Sim', 'Não'))
+    if valor == 'Sim':
+        x_tf[item] = 1
+    else:
+        x_tf[item] = 0
+
+for item in x_listas:
+    valor = st.selectbox(f'{item}', x_listas[item])
+    dicionario[f'{item}_{valor}'] = 1
+
+botao = st.button('Previsão de preço do imóvel')
+
